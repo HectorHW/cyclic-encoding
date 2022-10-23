e5628c435748cc471eb5bab223ba1bb9b9113e2a
Vladimir Redkin <vsredkin01@gmail.com>
2022-10-23 15:24:14 +0300

    Intial commit

---
+++ src/bits.rs
@@ -0,0 +1,140 @@
+use std::{
+    fmt::Display,
+    ops::{Deref, DerefMut},
+};
+
+use rand::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct BitVector<const SIZE: usize>(Vec<bool>);
+
+impl<const N: usize> Deref for BitVector<N> {
+    type Target = [bool];
+
+    fn deref(&self) -> &Self::Target {
+        self.0.as_slice()
+    }
+}
+
+impl<const N: usize> DerefMut for BitVector<N> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.0.as_mut_slice()
+    }
+}
+
+impl<const N: usize, T: num::Integer> TryFrom<Vec<T>> for BitVector<N> {
+    type Error = ();
+
+    fn try_from(value: Vec<T>) -> Result<Self, Self::Error> {
+        if value.len() != N {
+            return Err(());
+        }
+        Ok(BitVector(
+            value
+                .into_iter()
+                .map(|v| !v.is_zero())
+                .collect::<Vec<bool>>(),
+        ))
+    }
+}
+
+impl From<u8> for BitVector<8> {
+    fn from(mut item: u8) -> Self {
+        let mut buf = vec![];
+        for _ in 0..8 {
+            buf.push(item & 128 != 0);
+            item <<= 1;
+        }
+        BitVector(buf)
+    }
+}
+
+impl From<&BitVector<8>> for u8 {
+    fn from(val: &BitVector<8>) -> Self {
+        let mut result = 0u8;
+        let mut power = 128;
+        for &bit in &val[..] {
+            if bit {
+                result += power;
+            }
+            power >>= 1;
+        }
+        result
+    }
+}
+
+impl<const N: usize> Display for BitVector<N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for &item in &self[..] {
+            write!(f, "{}", item as u8)?;
+        }
+        Ok(())
+    }
+}
+
+pub fn break_one_bit(buf: &mut [bool]) -> usize {
+    debug_assert!(!buf.is_empty(), "break_one_bit on empty buffer");
+    let idx = thread_rng().gen_range(0..buf.len());
+    buf[idx] = !buf[idx];
+    idx
+}
+
+pub fn break_two_bits(buf: &mut [bool]) -> [usize; 2] {
+    debug_assert!(
+        buf.len() > 1,
+        "break_two_bits on empty or single-element buffer"
+    );
+    let mut rng = thread_rng();
+    let idx1 = rng.gen_range(0..buf.len());
+    let idx2 = loop {
+        let item = rng.gen_range(0..buf.len());
+        if item != idx1 {
+            break item;
+        }
+    };
+    buf[idx1] = !buf[idx1];
+    buf[idx2] = !buf[idx2];
+    [idx1, idx2]
+}
+
+pub fn break_n_bits(buf: &mut [bool], n_bits: usize) -> Vec<usize> {
+    debug_assert!(buf.len() >= n_bits);
+    let mut broken = vec![];
+    let mut rng = thread_rng();
+    for _ in 0..n_bits {
+        let this_bit = loop {
+            let item = rng.gen_range(0..buf.len());
+            if !broken.contains(&item) {
+                break item;
+            }
+        };
+
+        buf[this_bit] = !buf[this_bit];
+        broken.push(this_bit);
+    }
+    broken
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::BitVector;
+
+    pub fn convert_ascii(value: char) -> Result<BitVector<8>, ()> {
+        if !value.is_ascii() {
+            return Err(());
+        }
+
+        let byte = value as u8;
+
+        Ok(BitVector::from(byte))
+    }
+
+    #[test]
+    fn convert_w() {
+        let result = convert_ascii('w').unwrap();
+        assert_eq!(
+            result.as_ref(),
+            &vec![false, true, true, true, false, true, true, true]
+        )
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,185 @@
+use std::{
+    fmt::Write,
+    iter::{once, repeat},
+    ops::Mul,
+};
+
+use algebraics::polynomial::Polynomial;
+use bits::BitVector;
+use colored::*;
+use encoding_rs::WINDOWS_1251;
+
+use itertools::Itertools;
+
+use crate::bits::break_one_bit;
+
+mod bits;
+
+type Pol = Polynomial<i32>;
+
+fn into_cp1251_bits(letter: char) -> BitVector<8> {
+    let string_buf = String::from(letter);
+    let buf = WINDOWS_1251.encode(&string_buf).0.to_vec();
+    debug_assert_eq!(buf.len(), 1);
+    buf[0].into()
+}
+
+fn with_highlight(vec: &[bool], positions: &[usize]) -> String {
+    let mut buf = String::new();
+    for (i, digit) in vec.iter().cloned().enumerate() {
+        if positions.contains(&i) {
+            write!(buf, "{}", (digit as u8).to_string().red()).unwrap();
+        } else {
+            write!(buf, "{}", (digit as u8).to_string().white()).unwrap();
+        }
+    }
+    buf
+}
+
+fn into_polynomial<const N: usize>(bits: &BitVector<N>) -> Pol {
+    bits.iter().cloned().map(i32::from).collect()
+}
+
+fn into_bitvector<const N: usize>(p: Pol) -> BitVector<N> {
+    p.into_coefficients()
+        .into_iter()
+        .chain(repeat(0))
+        .take(N)
+        .collect_vec()
+        .try_into()
+        .unwrap()
+}
+
+fn shift_polynomial(pol: &Pol, shift: usize) -> Pol {
+    pol.mul(Polynomial::from(
+        repeat(0).take(shift).chain(once(1)).collect::<Vec<_>>(),
+    ))
+}
+
+fn wrap_mod_2(p: Pol) -> Pol {
+    let mut coefficients: Vec<_> = p.into_coefficients();
+    coefficients.iter_mut().for_each(|item| {
+        *item = item.rem_euclid(2);
+    });
+    while let Some(0) = coefficients.last() {
+        coefficients.pop();
+    }
+    coefficients.into()
+}
+
+fn add_mod2(x: &Pol, y: &Pol) -> Pol {
+    wrap_mod_2(x + y)
+}
+
+fn div_mod2(x: Pol, y: &Pol) -> (Pol, Pol) {
+    let (d, r) = x.div_rem(y);
+    (wrap_mod_2(d), wrap_mod_2(r))
+}
+
+#[cfg(feature = "verbose")]
+fn pretty_print_poly(p: &Pol) -> String {
+    let coeffincients = p.clone().into_coefficients();
+    if coeffincients.is_empty() || coeffincients == vec![0] {
+        return "0".to_string();
+    }
+    coeffincients
+        .into_iter()
+        .enumerate()
+        .fold(String::new(), |mut a, (power, coeff)| {
+            if coeff == 0 {
+                return a;
+            }
+
+            if a.is_empty() {
+                write!(
+                    a,
+                    "{}",
+                    match power {
+                        0 => "1".to_string(),
+                        1 => "x".to_string(),
+                        n => format!("x^{n}"),
+                    }
+                )
+                .unwrap();
+                a
+            } else {
+                write!(
+                    a,
+                    " + {}",
+                    match power {
+                        1 => "x".to_string(),
+                        n => format!("x^{n}"),
+                    }
+                )
+                .unwrap();
+                a
+            }
+        })
+}
+
+fn encode<const N: usize, const K: usize>(bits: BitVector<K>, base: &Pol) -> BitVector<N> {
+    #[cfg(feature = "verbose")]
+    println!("bits: {bits}");
+    let p = into_polynomial(&bits);
+    #[cfg(feature = "verbose")]
+    println!("polynomial: {}", pretty_print_poly(&p));
+    let shifted = shift_polynomial(&p, N - K);
+    #[cfg(feature = "verbose")]
+    println!("shifted polynomial: {}", pretty_print_poly(&shifted));
+    let (_, rem) = div_mod2(shifted.clone(), base);
+    #[cfg(feature = "verbose")]
+    println!("remainder: {}", pretty_print_poly(&rem));
+    let result = add_mod2(&rem, &shifted);
+    #[cfg(feature = "verbose")]
+    println!("shifted + remainder: {}", pretty_print_poly(&result));
+    let bits: BitVector<N> = into_bitvector(result);
+    #[cfg(feature = "verbose")]
+    println!("bits: {bits}");
+    bits
+}
+
+fn syndrome<const N: usize, const N_K: usize>(data: &BitVector<N>, base: &Pol) -> BitVector<N_K> {
+    into_bitvector(div_mod2(into_polynomial(data), base).1)
+}
+
+fn main() {
+    // 5 вариант - x^5 + x^4 + x^3 + x + 1
+
+    const N: usize = 13;
+    const K: usize = 8;
+
+    let g = Polynomial::from(vec![1, 1, 0, 1, 1, 1]);
+
+    assert_eq!(g.degree(), Some(N - K));
+
+    let alphabet = ['л', 'м', 'н', 'о', 'п'];
+
+    let mut codes = vec![];
+
+    for (code, &letter) in alphabet
+        .iter()
+        .cloned()
+        .map(into_cp1251_bits)
+        .zip(alphabet.iter())
+    {
+        let encoded = encode::<N, K>(code.clone(), &g);
+        println!("{letter} => {} => {}", code, encoded);
+        codes.push(encoded);
+    }
+
+    for encoded in &codes {
+        let s = syndrome::<N, { N - K }>(encoded, &g);
+        println!("encoded: {}, syndrome: {}", encoded, s);
+    }
+
+    for encoded in &codes {
+        let mut with_bit_broken = encoded.clone();
+        let idx = break_one_bit(&mut with_bit_broken);
+        let s = syndrome::<N, { N - K }>(&with_bit_broken, &g);
+        println!(
+            "{} syndrome: {}",
+            with_highlight(&with_bit_broken, &[idx]),
+            s
+        );
+    }
+}
